bb9588584ef351027121958054cba04e0f7e0c8e
RE0x2A <rend42@163.com>
2023-01-12 11:33:57 +0800

    init

---
+++ theme.config.jsx
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+export default {
+  logo: <span>Talentsec 公开文档</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra',
+  },
+  toc: {
+    extraContent: <Fragment />,
+  },
+}
