2e4fc615e6f73414bf9c0a3d520bd0415e03cb49
RE0x2A <rend42@163.com>
2023-01-13 13:49:56 +0800

    update ci

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
 })
 
 module.exports = withNextra({
+  assetPrefix: isProd ? '/open-docs' : '',
   images: {
     unoptimized: true,
   },
