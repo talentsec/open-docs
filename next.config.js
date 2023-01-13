0624e4e9be91f7ed21add19b0ec7a2f99d184ea5
RE0x2A <rend42@163.com>
2023-01-13 14:27:43 +0800

    update config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
-const isProd = process.env.NODE_ENV === 'production'
+/** @type {import('next').NextConfig} */
 
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
 })
 
-module.exports = withNextra({
-  assetPrefix: isProd ? '/open-docs' : '',
+let config = {
   images: {
     unoptimized: true,
   },
-})
+}
+
+if (process.env.GITHUB_REPOSITORY) {
+  const repositoryName = process.env.GITHUB_REPOSITORY.split('/')[1]
+
+  config = {
+    ...config,
+    assetPrefix: `/${repositoryName}/`,
+    basePath: `/${repositoryName}`,
+  }
+}
+
+module.exports = withNextra(config)
